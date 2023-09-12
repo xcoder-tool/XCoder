37e8c296b780c3ac1905857024c0855a4b7dd02c
Danila <crowo4ka@gmail.com>
2023-09-12 19:47:31 +0300

    refactor: directories creation extracted to a method

--- system/lib/features/directories.py
+++ system/lib/features/directories.py
@@ -2,17 +2,34 @@ import os
 import shutil
 
 
+def create_directories():
+    for io_type in ("In", "Out"):
+        for filetype in ("Compressed", "Decompressed", "Sprites"):
+            os.makedirs(f"SC/{io_type}-{filetype}", exist_ok=True)
+
+        for filetype in ("Compressed", "Decompressed"):
+            os.makedirs(f"CSV/{io_type}-{filetype}", exist_ok=True)
+
+        for filetype in ("KTX", "PNG"):
+            os.makedirs(f"TEX/{io_type}-{filetype}", exist_ok=True)
+
+
 def clear_directories():
-    for i in ["In", "Out"]:
-        for k in ["Compressed", "Decompressed", "Sprites"]:
-            folder = f"SC/{i}-{k}"
+    for io_type in ("In", "Out"):
+        for filetype in ("Compressed", "Decompressed", "Sprites"):
+            folder = f"SC/{io_type}-{filetype}"
             if os.path.isdir(folder):
                 shutil.rmtree(folder)
             os.makedirs(folder, exist_ok=True)
 
-    for i in ["In", "Out"]:
-        for k in ["Compressed", "Decompressed"]:
-            folder = f"CSV/{i}-{k}"
+        for filetype in ("Compressed", "Decompressed"):
+            folder = f"CSV/{io_type}-{filetype}"
+            if os.path.isdir(folder):
+                shutil.rmtree(folder)
+            os.makedirs(folder, exist_ok=True)
+
+        for filetype in ("KTX", "PNG"):
+            folder = f"TEX/{io_type}-{filetype}"
             if os.path.isdir(folder):
                 shutil.rmtree(folder)
             os.makedirs(folder, exist_ok=True)
