ffa135851c5368fed9041f74a93b8b4223c7b39d
Danila <crowo4ka@gmail.com>
2023-09-23 17:06:17 +0300

    refactor: directories recreate directory method, type constants

--- system/lib/features/directories.py
+++ system/lib/features/directories.py
@@ -1,35 +1,38 @@
 import os
 import shutil
 
+IO_TYPES = ("In", "Out")
+
+SC_FILE_TYPES = ("Compressed", "Decompressed", "Sprites")
+CSV_FILE_TYPES = ("Compressed", "Decompressed")
+TEXTURE_FILE_TYPES = ("KTX", "PNG")
+
 
 def create_directories():
-    for io_type in ("In", "Out"):
-        for filetype in ("Compressed", "Decompressed", "Sprites"):
+    for io_type in IO_TYPES:
+        for filetype in SC_FILE_TYPES:
             os.makedirs(f"SC/{io_type}-{filetype}", exist_ok=True)
 
-        for filetype in ("Compressed", "Decompressed"):
+        for filetype in CSV_FILE_TYPES:
             os.makedirs(f"CSV/{io_type}-{filetype}", exist_ok=True)
 
-        for filetype in ("KTX", "PNG"):
+        for filetype in TEXTURE_FILE_TYPES:
             os.makedirs(f"TEX/{io_type}-{filetype}", exist_ok=True)
 
 
 def clear_directories():
-    for io_type in ("In", "Out"):
-        for filetype in ("Compressed", "Decompressed", "Sprites"):
-            folder = f"SC/{io_type}-{filetype}"
-            if os.path.isdir(folder):
-                shutil.rmtree(folder)
-            os.makedirs(folder, exist_ok=True)
+    for io_type in IO_TYPES:
+        for filetype in SC_FILE_TYPES:
+            _recreate_directory(f"SC/{io_type}-{filetype}")
 
-        for filetype in ("Compressed", "Decompressed"):
-            folder = f"CSV/{io_type}-{filetype}"
-            if os.path.isdir(folder):
-                shutil.rmtree(folder)
-            os.makedirs(folder, exist_ok=True)
+        for filetype in CSV_FILE_TYPES:
+            _recreate_directory(f"CSV/{io_type}-{filetype}")
 
-        for filetype in ("KTX", "PNG"):
-            folder = f"TEX/{io_type}-{filetype}"
-            if os.path.isdir(folder):
-                shutil.rmtree(folder)
-            os.makedirs(folder, exist_ok=True)
+        for filetype in TEXTURE_FILE_TYPES:
+            _recreate_directory(f"TEX/{io_type}-{filetype}")
+
+
+def _recreate_directory(directory):
+    if os.path.isdir(directory):
+        shutil.rmtree(directory)
+    os.makedirs(directory, exist_ok=True)
